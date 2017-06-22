000245e1f4036670b9265dcab9c09da68443498f
Samy <samysamuel000@gmail.com>
2017-06-22 16:18:06 +0300

    Create start.cmd

---
+++ start.cmd
@@ -0,0 +1,30 @@
+@echo off
+title SkyLightPM server software for Minecraft: Pocket Edition
+mode 1000
+color 0a
+cd /d %~dp0
+
+if exist bin\php\php.exe (
+	set PHPRC=""
+	set PHP_BINARY=bin\php\php.exe
+) else (
+	set PHP_BINARY=php
+)
+
+if exist SkyLightPM.phar (
+	set POCKETMINE_FILE=SkyLightPM.phar
+) else (
+	if exist src\pocketmine\PocketMine.php (
+		set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+	) else (
+		echo "Couldn't find a valid SkyLightPM installation"
+		pause
+		exit 1
+	)
+)
+
+if exist bin\mintty.exe (
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "SkyLightPM" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+) else (
+	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+)
